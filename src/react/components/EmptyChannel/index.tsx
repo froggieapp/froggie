61ae327b493efb220994b27b8f61fadedf3ab6dc
v <153395228+vicodinee@users.noreply.github.com>
2023-12-24 16:47:58 -0300

    fix empty channel screen callback

--- src/react/components/EmptyChannel/index.tsx
+++ src/react/components/EmptyChannel/index.tsx
@@ -1,10 +1,16 @@
 import { PlusCircleIcon } from "@heroicons/react/24/solid";
 import { h } from "preact";
 import "./index.css";
+import { onOpen } from "../MicroModalWrapper";
+import { ADD_CHANNEL_MODAL } from "@/react/util/modals";
 
 export const EmptyChannel = () => {
+  const onClick = () => {
+    onOpen(ADD_CHANNEL_MODAL);
+  };
+  
   return (
-    <button type="button" className="empty-channel">
+    <button onClick={onClick} type="button" className="empty-channel">
       <p className="empty-channel-heading">Add a channel</p>
       <p className="empty-channel-alt">Seems like it&apos;s your first time in here, click here to add a new channel</p>
       <PlusCircleIcon className="plus-channel-icon" />
