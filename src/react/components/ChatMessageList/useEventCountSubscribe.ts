a32ad9952e8333c860c8c277b8888f3d7f8bb805
v <153395228+vicodinee@users.noreply.github.com>
2023-12-24 16:07:25 -0300

    fix zustand usage

--- src/react/components/ChatMessageList/useEventCountSubscribe.ts
+++ src/react/components/ChatMessageList/useEventCountSubscribe.ts
@@ -1,7 +1,8 @@
-import { StoreState, useStore } from "@/react/store/Store";
+import { useStore } from "@/react/store/Store";
 import { StoreEvent } from "@/react/store/createEventStore";
 import debounce from "debounce";
 import { useEffect, useState } from "preact/hooks";
+import { shallow } from "zustand/shallow";
 
 export const useEventCountSubscribe = (channelId: string | undefined) => {
   const [eventCount, setEventCount] = useState(() => useStore.getState().getChannelEvents(channelId).length);
@@ -13,12 +14,21 @@ export const useEventCountSubscribe = (channelId: string | undefined) => {
     };
     const currentEventCount = useStore.getState().getChannelEvents(channelId).length;
     setEventCount(currentEventCount);
-    const debounceCb = debounce((state: StoreState) => {
-      const events = state.getChannelEvents(channelId);
-      updateState(events.length);
-      setLastEvent(events.length ? events[events.length - 1] : null);
+    const debounceCb = debounce(({ count, lastEvent }: { count: number; lastEvent: StoreEvent | null }) => {
+      updateState(count);
+      setLastEvent(lastEvent);
     }, 50);
-    const subscribe = useStore.subscribe(debounceCb);
+    const subscribe = useStore.subscribe(
+      (state) => {
+        const events = state.getChannelEvents(channelId);
+        return {
+          count: events.length,
+          lastEvent: events.length ? events[events.length - 1] : null,
+        };
+      },
+      debounceCb,
+      { equalityFn: shallow },
+    );
     return () => {
       debounceCb.clear();
       subscribe();
